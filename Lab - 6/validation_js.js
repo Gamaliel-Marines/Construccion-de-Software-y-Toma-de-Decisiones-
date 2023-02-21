ebac180affffd3216c7e84f3c7121b545a2d7b5d
Gamaliel-Marines <gamaliel_projects@hotmail.com>
2023-02-21 12:16:15 -0600

    new index

---
+++ Lab - 6/validation_js.js
@@ -0,0 +1,51 @@
+function validatePassword()
+{
+    let password = document.getElementById("password").value;
+    let validatePassword = document.getElementById("validate-password").value;
+    let message = document.getElementById("message");
+
+    if (password != validatePassword)
+    {
+        message.innerHTML = "Passwords dont match";
+        return false;
+    } 
+    
+    else if (password == validatePassword && password != "" && validatePassword != "")
+    {
+        message.innerHTML = "Passwords match";
+        return true;
+    }
+}
+
+let font = document.getElementById("font");
+
+font.onmousewheel = () => 
+{
+    const colores =
+    [
+        "text-primary",
+        "text-secondary",
+        "text-success",
+        "text-danger",
+        "text-warning",
+        "text-info",
+        "text-light",
+        "text-dark"
+    ]
+    const index = Math.floor(Math.random() * colores.length);
+    font.classList = ["d-flex justify-content-center align-items-center"];
+    font.classList.add(colores[index]);
+}
+
+let password = document.getElementById("password");
+let popup = document.getElementById("myPopup");
+
+password.onmouseover = () =>
+{
+    popup.classList.toggle("show");
+}
+
+password.onmouseout = () =>
+{
+    popup.classList.toggle("show");
+}
