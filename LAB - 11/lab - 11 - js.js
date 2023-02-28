c938112bdbd51fddd16d4168a8626ddc43dc5348
Gamaliel-Marines <gamaliel_projects@hotmail.com>
2023-02-28 13:20:42 -0600

    lab11

--- LAB - 11/lab - 11 - js.js
+++ LAB - 11/lab - 11 - js.js
@@ -1,50 +1,34 @@
 const express = require('express');
-const app = express();
-//Middleware
-app.use((request, response, next) => {
-    console.log('Middleware!');
-    //Le permite a la petición avanzar hacia el siguiente middleware
-    next(); 
-});
-app.use('/home', (request, response, next) => {
-    response.send('Bienvenido a casa!'); 
-});
-app.use('/pedir', (request, response, next) => {
-    let html = `
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1>Hot cakes</h1>
-                <form action="/pedir" method="POST">
-                    <fieldset>
-                        <legend>Escoge tu desayuno:</legend>
-                        <div>
-                            <input type="checkbox" id="malteada" name="malteada">
-                            <label for="malteada">Malteada</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" id="cafe" name="cafe">
-                            <label for="cafe">Café</label>
-                        </div>
-                        <div>
-                            <input type="number" id="hot_cakes" name="hot_cakes" value="0" min="0">
-                            <label for="hot_cakes"> hot cakes</label>
-                        </div>
-                    </fieldset>
-                    <br>
-                    <input type="submit" value="pedir">
-                </form>
-            </body>
-        </html>
-    `;
-    response.send(html);
-});
-app.use((request, response, next) => {
-    console.log('Otro middleware!');
-    //Manda la respuesta
-    response.send('¡Hola mundo!'); 
-});
-app.listen(3000);
+ const bodyParser = require('body-parser');
+ 
+ const app = express();
+ 
+ app.use(bodyParser.urlencoded({extended: false}));
+ 
+ //Middleware
+ app.use((request, response, next) => {
+     console.log('Middleware!');
+ 
+     //Le permite a la petición avanzar hacia el siguiente middleware
+     next(); 
+ });
+ 
+ app.use('/home', (request, response, next) => {
+ 
+     response.send('Bienvenido a casa!'); 
+ });
+ 
+ const hotcakesRutas = require('./routes/hot_cakes.routes');
+ 
+ app.use('/hot_cakes', hotcakesRutas);
+ 
+ app.use((request, response, next) => {
+     console.log('Otro middleware!');
+ 
++    response.status(404);
++
+     //Manda la respuesta 
++    response.send('Lo sentimos, ya no tenemos hot cakes'); 
+ });
+ 
+ app.listen(3000);
